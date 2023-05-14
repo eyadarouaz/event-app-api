9fd2089ac2878daa9488f3af05fc866807d7010d
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 17:32:18 +0100

    style: lint code and indentation

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,12 +1,12 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const ormConfig : TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'database',
-    database: 'events',
-    autoLoadEntities: true,
-    synchronize: true,
-  }
+export const ormConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'database',
+  database: 'events',
+  autoLoadEntities: true,
+  synchronize: true,
+};
