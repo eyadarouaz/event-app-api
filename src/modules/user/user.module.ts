1cb83fd5535fb3a8b827a739f2d03897aa0b727a
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 16:56:30 +0100

    style: lint code

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,17 +1,14 @@
 import { UserController } from './user.controller';
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
-import { UserService } from "./user.service";
+import { UserService } from './user.service';
 import { AdminStrategy } from '../auth/admin.strategy';
 
-
 @Module({
-    imports: [
-      TypeOrmModule.forFeature([User]),
-    ],
-    controllers: [UserController],
-    providers: [UserService, AdminStrategy],
-    exports: [UserService]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [UserService, AdminStrategy],
+  exports: [UserService],
 })
 export class UserModule {}
