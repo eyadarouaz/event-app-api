2050ac5d1dd343e5544c3fc8f18833a34ad0cc13
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 16:51:18 +0100

    style: lint code

--- src/shared/interceptors/response.interceptor.ts
+++ src/shared/interceptors/response.interceptor.ts
@@ -1,13 +1,20 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor, HttpStatus  } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  HttpStatus,
+} from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map(value => {
-        value = (value) ? value : []
-        return { statusCode: HttpStatus.OK, message: "success",  data: value};
-      }));
+      map((value) => {
+        value = value ? value : [];
+        return { statusCode: HttpStatus.OK, message: 'success', data: value };
+      }),
+    );
   }
 }
