fbdddc4035a50a147d258ca380d60195134e68e2
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 17:15:30 +0100

    refactor: create post automatically when creating a survey or event

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,40 +1,26 @@
+import {
+  Controller
+} from '@nestjs/common';
+import {
+  Get,
+  UseGuards
+} from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { CreatePostDto } from './dto/create-post.dto';
-import { Controller, ParseIntPipe, Post, Request, ValidationPipe } from "@nestjs/common";
-import { Body, Delete, Get, Param, Put, UseGuards, UseInterceptors } from "@nestjs/common/decorators";
-import { PostService } from "./post.service";
-import { UserService } from 'src/modules/user/user.service';
 import { ApiTags } from '@nestjs/swagger';
+import { UserService } from 'src/modules/user/user.service';
+import { PostService } from './post.service';
 
 @ApiTags('post')
 @UseGuards(AuthGuard())
 @Controller('post')
 export class PostController {
-    constructor(private readonly postService: PostService,
-        private readonly userService: UserService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly userService: UserService,
+  ) {}
 
-    @Get()
-    async getPosts() {
-        return this.postService.getAllPosts();
-    }
-
-    @Post()
-    async createPost(@Request() req, @Body(new ValidationPipe()) createDto: CreatePostDto) {
-        const user = await this.userService.getUserById(req.user.id);
-        console.log(user);
-        return this.postService.createPost(user, createDto);
-    }
-
-    @Put(':id')
-    async updatePost(@Request() req, @Param('id', ParseIntPipe) id: number,
-                     @Body(new ValidationPipe()) updateDto: UpdatePostDto ) {
-       return this.postService.updatePost(req.user, id, updateDto);
-    }
-    
-    @Delete(':id')
-    async deletePost(@Request() req, @Param('id', ParseIntPipe) id: number) {
-        return this.postService.deletePost(req.user, id);
-    }
-
+  @Get()
+  async getPosts() {
+    return this.postService.getAllPosts();
+  }
 }
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,80 +1,45 @@
-import { UpdatePostDto } from './dto/update-post.dto';
-import { User } from 'src/entities/user.entity';
-import { CreatePostDto } from './dto/create-post.dto';
+import {
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ForbiddenException, HttpException, HttpStatus, Injectable, NotFoundException} from "@nestjs/common";
-import { Repository } from 'typeorm';
 import { Post } from 'src/entities/post.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
-    constructor( @InjectRepository(Post) private postsRepository: Repository<Post>) {}
+  constructor(
+    @InjectRepository(Post) private postsRepository: Repository<Post>,
+  ) {}
 
-    
-    async createPost( user: User, postDto: CreatePostDto) {
-        try {
-            const post = await this.postsRepository.create({...postDto, user: user });
-            await this.postsRepository.save(post);
-            return post;
-        }catch(err) {
-            throw new HttpException("Cannot create post", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  //Getters
+  async getAllPosts() {
+    return await this.postsRepository.find({
+      relations: { eventPost: true, user: true, surveyPost: { options: true } },
+    });
+  }
+
+  async getPostById(id: number) {
+    try {
+      return await this.postsRepository.findOne({
+        where: { id: id },
+      });
+    } catch {
+      throw new NotFoundException('No posts found');
     }
+  }
 
-    async updatePost(user:User, id: number, updateDto: UpdatePostDto) {
-        const toUpdate = await this.postsRepository.findOne({
-            relations: {user: true}, 
-            where: {
-                user: user, id:id
-            }
-        });
-        if(toUpdate) {
-            return await this.postsRepository.update({id:id, user:user}, {...updateDto});
-        }
-        throw new ForbiddenException("Cannot update post");
+  async getPostByUser(user: User) {
+    try {
+      return this.postsRepository.find({
+        relations: { user: true },
+        where: {
+          user: { id: user.id },
+        },
+      });
+    } catch (err) {
+      throw new NotFoundException('No posts found');
     }
-
-    async deletePost(user: User, id: number) {
-        const toDelete = await this.postsRepository.findOne({
-            relations: {user: true}, 
-            where: {
-                user: user, id:id
-            }
-        });
-        if (toDelete){
-            return await this.postsRepository.delete(id);
-        }
-        throw new ForbiddenException("Cannot delete post");
-    }
-
-    //Getters
-    async getAllPosts() {
-        return await this.postsRepository.find();
-    }
-
-    async getPostById(id: number) {
-        try {
-            return await this.postsRepository.findOne({
-                where: {id: id}
-            });
-        }catch {
-            throw new NotFoundException('No posts found');
-        }
-    }
-
-    async getPostByUser(user: User){
-        try {
-            return this.postsRepository.find({
-                relations: {user: true}, 
-                where: {
-                    user: {id: user.id}
-                }
-            });
-        } catch (err) {
-            throw new NotFoundException('No posts found');
-        }
-    }
-
-    
+  }
 }
-
