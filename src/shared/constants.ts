2918257e3e5b5170647162dd52e4e69c98185f3b
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 16:49:53 +0100

    style: fix indentation

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,8 +1,8 @@
 export const jwtConstant = {
-    secret: 'wjeld-djeuedw399e3-uejheuii33-4jrjjejei3-rjdjfjf',
-}
+  secret: 'wjeld-djeuedw399e3-uejheuii33-4jrjjejei3-rjdjfjf',
+};
 
 export const salt = 10;
 export const strategies = {
-    admin: 'ADMIN' 
+  admin: 'ADMIN' 
 }
