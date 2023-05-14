fb355481b4348628dcc111040693c7523090400e
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-14 17:17:37 +0100

    style: lint code

--- src/modules/like/like.controller.ts
+++ src/modules/like/like.controller.ts
@@ -1,34 +1,41 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Request,
+  UseGuards
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { LikeService } from './like.service';
-import { Controller, Post, UseGuards, Request, Param, Delete, UseInterceptors, Get, ParseIntPipe } from "@nestjs/common";
-import { UserService } from "src/modules/user/user.service";
-import { ResponseInterceptor } from 'src/shared/interceptors/response.interceptor';
 import { ApiTags } from '@nestjs/swagger';
+import { UserService } from 'src/modules/user/user.service';
+import { LikeService } from './like.service';
 
 @ApiTags('like')
 @UseGuards(AuthGuard())
 @Controller()
 export class LikeController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly likeService: LikeService,
+  ) {}
 
-    constructor(private readonly userService: UserService,
-        private readonly likeService: LikeService) {}
-    
-    
-    @Get(':id/likes')
-    async getLikesByPost(@Param('id', ParseIntPipe) id: number) {
-        return this.likeService.getLikesByPost(id);
-    }
+  @Get(':id/likes')
+  async getLikesByPost(@Param('id', ParseIntPipe) id: number) {
+    return this.likeService.getLikesByPost(id);
+  }
 
-    @Post(':id/like')
-    async likePost(@Request() req, @Param('id', ParseIntPipe) postId: number) {
-        const user = await this.userService.getUserById(req.user.id);
-        console.log(user);
-        return this.likeService.likePost(user, postId);
-    }
+  @Post(':id/like')
+  async likePost(@Request() req, @Param('id', ParseIntPipe) postId: number) {
+    const user = await this.userService.getUserById(req.user.id);
+    console.log(user);
+    return this.likeService.likePost(user, postId);
+  }
 
-    @Delete(':id/dislike')
-    async dislikePost(@Request() req, @Param('id', ParseIntPipe) postId: number) {
-        return this.likeService.dislikePost(req.user, postId);
-    }       
-    
-}        
+  @Delete(':id/dislike')
+  async dislikePost(@Request() req, @Param('id', ParseIntPipe) postId: number) {
+    return this.likeService.dislikePost(req.user, postId);
+  }
+}
--- src/modules/like/like.module.ts
+++ src/modules/like/like.module.ts
@@ -1,20 +1,21 @@
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from './../user/user.module';
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { JwtStrategy } from "src/modules/auth/jwt.strategy";
-import { Like } from "src/entities/like.entity";
-import { LikeController } from "./like.controller";
-import { LikeService } from "./like.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtStrategy } from 'src/modules/auth/jwt.strategy';
+import { Like } from 'src/entities/like.entity';
+import { LikeController } from './like.controller';
+import { LikeService } from './like.service';
 import { PostModule } from 'src/modules/post/post.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Like]),
-    PassportModule.register({defaultStrategy: 'jwt'}),
+  imports: [
+    TypeOrmModule.forFeature([Like]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     UserModule,
-    PostModule],
-    controllers: [LikeController],
-    providers: [LikeService, JwtStrategy],
-  })
-  export class LikeModule {}
-  
+    PostModule,
+  ],
+  controllers: [LikeController],
+  providers: [LikeService, JwtStrategy],
+})
+export class LikeModule {}
