814939ff7a3a8bcfc0a4a46083c1fa6bb82a168a
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-28 20:46:48 +0100

    feat: connect and disconnect users, get all connected users (wip)

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,28 +1,44 @@
-import { Message } from './../../entities/message.entity';
-import { User } from './../../entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Socket } from 'socket.io';
+import { Status } from 'src/shared/enums/status.enum';
+import { Repository } from 'typeorm';
 import { AuthService } from '../auth/auth.service';
+import { Message } from './../../entities/message.entity';
+import { User } from './../../entities/user.entity';
 
 @Injectable()
 export class ChatService {
   constructor(
     @InjectRepository(Message) private messagesRepository: Repository<Message>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
     private authService: AuthService,
   ) {}
 
   async getUserFromSocket(socket: Socket) {
-    const token = socket.handshake.headers.authorization;
+    const token = await socket.handshake.query.token;
     const payload = await this.authService.verifyToken(token);
     return payload.user;
+  }
+
+  async getAllActive() {
+    return await this.usersRepository.find({
+      where: { status: Status.ONLINE },
+    });
   }
 
   async createMessage(content: string, user: User): Promise<Message> {
     const message = await this.messagesRepository.create({ content, user });
     await this.messagesRepository.save(message);
     return message;
+  }
+
+  async connectUser(id: number) {
+    return this.usersRepository.update({ id: id }, { status: Status.ONLINE });
+  }
+
+  async disconnectUser(id: number) {
+    return this.usersRepository.update({ id: id }, { status: Status.OFFLINE });
   }
 
   async getMessages(): Promise<Message[]> {
